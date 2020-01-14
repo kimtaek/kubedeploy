2c1ce718e28f2bb2a4b47b6ee32ad982625c70cf
JIN ZE <jinze1991@icloud.com>
2020-01-14 15:41:30 +0900

    init repository

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ${PLUGIN_TOKEN} | base64 -d > token
+echo ${PLUGIN_CERT} | base64 -d > ca
+
+kubectl config set-cluster default --server=${PLUGIN_SERVER} --certificate-authority="/ca"
+kubectl config set-credentials ${PLUGIN_SERVICE_ACCOUNT} --token="$(cat /token)"
+kubectl config set-context default --cluster=default --user=${PLUGIN_SERVICE_ACCOUNT}
+kubectl config use-context default
+
+kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${PLUGIN_DEPLOYMENT} ${PLUGIN_CONTAINER}=${PLUGIN_REPOSITORY}:${PLUGIN_TAG} --record
+
+SH=$(kubectl config unset users.${PLUGIN_SERVICE_ACCOUNT})
+SH=$(kubectl config unset contexts.default)
+SH=$(kubectl config unset clusters.default)
+SH=$(rm -rf /root/.kube)
---
+++ readme.md
@@ -0,0 +1,15 @@
+### usage
+
+```bash
+docker run --rm \
+   -e PLUGIN_NAMESPACE=develop \
+   -e PLUGIN_SERVICE_ACCOUNT=deploy \
+   -e PLUGIN_TOKEN=ZXlKaGJHY2lPaUpTVXp... \
+   -e PLUGIN_SERVER=https://D34...DE.ap-northeast-2.eks.amazonaws.com \
+   -e PLUGIN_CERT=LS0tLS1... \
+   -e PLUGIN_DEPLOYMENT=api \
+   -e PLUGIN_CONTAINER=api \
+   -e PLUGIN_REPOSITORY=my/repository \
+   -e PLUGIN_TAG=hash \
+   kubedeploy
+```
